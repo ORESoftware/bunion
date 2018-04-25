7f23065a77902dae26da8cd25552983a5cb7339b
Olegzandr VD <alex@oresoftware.com>
2018-04-24 23:52:48 -0700

    pdev:set

--- lib/cli.ts
+++ lib/cli.ts
@@ -31,18 +31,28 @@ const options = [
     names: ['output', 'o'],
     type: 'string',
     default: 'short'
-  }
+  },
+  {
+    names: ['match', 'or'],
+    type: 'arrayOfString',
+    default: [] as Array<string>
+  },
+  {
+    names: ['must-match', 'and'],
+    type: 'arrayOfString',
+    default: [] as Array<string>
+  },
+
 ];
 
 let opts, parser = dashdash.createParser({options: options});
 
 try {
-   opts = parser.parse(process.argv);
+  opts = parser.parse(process.argv);
 } catch (e) {
   log.error('bunion: error: %s', e.message);
   process.exit(1);
 }
-
 
 // Use `parser.help()` for formatted options help.
 if (opts.help) {
@@ -51,41 +61,90 @@ if (opts.help) {
   process.exit(0);
 }
 
+const flattenDeep = function (a: Array<string>): Array<string> {
+  return a.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+};
+
 const level = opts.level;
 const output = opts.output;
 const maxLevel = String(level || 'trace').toUpperCase();
 const maxIndex = ordered.indexOf(maxLevel);
+const andMatches = flattenDeep([opts.must_match]).filter(v => v).map(v => new RegExp(v, 'g'));
+const orMatches = flattenDeep([opts.match]).filter(v => v).map(v => new RegExp(v, 'g'));
 
 if (maxIndex < 0) {
   throw new Error('Your value for env var "bunion_max_level" is not set to a valid value (\'WARN\' | \'INFO\' | \'DEBUG\' | \'ERROR\' | \'TRACE\')');
 }
 
 
+const matches = function (v: string) {
+  if (orMatches.length < 1) {
+    return true;
+  }
+  
+  return orMatches.some(function (m) {
+    return m.test(v);
+  });
+};
+
+const mustMatches = function (v: string) {
+  return andMatches.every(function (m) {
+    return m.test(v);
+  });
+};
+
+const allMatches = andMatches.concat(orMatches);
+
+const getHighlightedString = function (str: string) {
+  return allMatches.map(function (v) {
+    return str.replace(v, function replacer(match, p1, p2, p3, offset, string) {
+      // p1 is nondigits, p2 digits, and p3 non-alphanumerics
+      return chalk.yellow.bold(match);
+    });
+  })
+  .join('');
+};
+
 process.stdin.resume().pipe(createParser())
 .on('bunion-json', function (v: BunionJSON) {
-  // console.log(v.date, v.appName, v.level, v.value);
   
-  if(v.level === 'ERROR'){
+  if (!(matches(v.value) && mustMatches(v.value))) {
+    return;
+  }
+  
+  // if (!mustMatches(v.value)) {
+  //   console.log('did not match AND:', v.value);
+  //   return;
+  // }
+  //
+  // if (!matches(v.value)) {
+  //   console.log('did not match OR:', v.value);
+  //   return;
+  // }
+  
+  v.value = getHighlightedString(v.value);
+  
+  if (v.level === 'ERROR') {
     process.stderr.write(`${v.date} ${v.appName} ${chalk.red(v.level)} ${chalk.black.bold(v.value)} \n`);
     return;
   }
   
-  if(v.level === 'WARN' && maxIndex < 4){
+  if (v.level === 'WARN' && maxIndex < 4) {
     process.stderr.write(`${v.date} ${v.appName} ${chalk.magentaBright(v.level)} ${chalk.black.bold(v.value)} \n`);
     return;
   }
   
-  if(v.level === 'DEBUG' && maxIndex < 3){
+  if (v.level === 'DEBUG' && maxIndex < 3) {
     process.stdout.write(`${v.date} ${v.appName} ${chalk.gray(v.level)} ${chalk.blue(v.value)} \n`);
     return;
   }
   
-  if(v.level === 'INFO' && maxIndex < 2){
+  if (v.level === 'INFO' && maxIndex < 2) {
     process.stdout.write(`${v.date} ${v.appName} ${chalk.gray(v.level)} ${chalk.cyan.bold(v.value)} \n`);
     return;
   }
   
-  if(v.level === 'TRACE' && maxIndex < 1){
+  if (v.level === 'TRACE' && maxIndex < 1) {
     process.stdout.write(`${v.date} ${v.appName} ${chalk.gray(v.level)} ${chalk.gray(v.value)} \n`);
     return;
   }
