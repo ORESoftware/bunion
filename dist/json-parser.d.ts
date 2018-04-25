3265e482314a9eb6db14bbe00a9f5ed782e5746f
Olegzandr VD <alex@oresoftware.com>
2018-04-24 22:31:30 -0700

    0.0.105

--- dist/json-parser.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import * as stream from 'stream';
-export interface IParsedObject {
-    [index: string]: any;
-}
-export declare const createParser: () => stream.Transform;
-export default createParser;
