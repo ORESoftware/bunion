2b19701f67829bf8c439dc1f3fd5fb871a11f718
Olegzandr VD <alex@oresoftware.com>
2018-05-01 18:04:03 -0700

    pdev:set

--- test/speed/bunion.js
+++ test/speed/bunion.js
@@ -9,8 +9,10 @@ const log2 = log.child().addField('mark','rubio');
 
 
 for(let i = 0; i < 100000; i++){
+  Math.random() > 0.2 && console.log('foo biz baz');
   log.info('booty');
   log.warn('biggo');
+  Math.random() > 0.2 && console.error(' 1 2 3');
   log2.info('sam');
   log.error('zzzz');
 }
