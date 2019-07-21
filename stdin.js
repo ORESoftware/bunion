1ffde85c07fe7f2b8b8a6da955922f1f34273d56
Alexander Mills <alex@alexs-mac.local>
2019-07-20 23:44:26 -0700

    tmp

--- stdin.js
+++ stdin.js
@@ -18,10 +18,10 @@ const v = '[1,2,3,'; // 2992
 
 const d = Date.now();
 
-for (let i = 0; i < 100000; i++) {
+for (let i = 0; i < 1000000; i++) {
   // const c = v + '"3":4}';
   const c = v + '4]';
-  JSON.stringify(JSON.parse(c));
+  console.log(JSON.stringify(JSON.parse(c)));
 }
 
 console.log(Date.now() - d);
